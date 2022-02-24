f236483aa37e4fc33b1f00b67abdb1ad1819ff69
Thomas McLennan <tgmclennan@hotmail.com>
2022-02-24 11:18:06 -0800

    feat: add firebase to angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'IzaSyCKSPzM6xNGmpQr64dbrafkJ-NPWGoQ8Sk',
+    authDomain: 'bcssa-mark.firebaseapp.com',
+    projectId: 'bcssa-mark',
+    storageBucket: 'bcssa-mark.appspot.com',
+    messagingSenderId: '678171852429',
+    appId: '1:678171852429:web:137c86a6b473943a4eecc4'
+  }
 };
 
 /*
